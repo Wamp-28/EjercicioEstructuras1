0f5fced03d58ba8b6f2cbc7fe2fc5eb3bad849ad
AlexM <wmatallana28@gmail.com>
2025-02-17 09:22:58 -0500

    Configuracion

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+
+
+
+        System.out.println("Estamos aprendiendo git");
+        System.out.println("Estamos repasando los comandos");
+
+
+
+    }
+}
